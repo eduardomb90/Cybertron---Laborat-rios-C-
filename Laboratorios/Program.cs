3cd31c53875cc14d2438cf9336d45641a48b3598
emarquba <eduardo.marquesbarbosa@nttdata.com>
2021-11-10 10:45:52 -0300

    Concluindo o lab4

--- Laboratorios/Program.cs
+++ Laboratorios/Program.cs
@@ -6,12 +6,7 @@ namespace Laboratorios
     {
         static void Main(string[] args)
         {
-            //Laboratorio3.ArrayInteiro();
-            //Laboratorio3.ArrayString();
-            //Laboratorio3.ArrayDateTime();
-            //ExercicioUm.CopiaArray();
-            //ExercicioDois.SomaColunasArrayMultiDimensional();
-            ExercicioDois.SomaColunasArrayJagged();
+            ContaCorrente.ExerciciosUmDoisTres();
         }
     }
 }
